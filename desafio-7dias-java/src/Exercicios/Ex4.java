ba754e216930c597a88f834d399028967acc8a33
daniela.r03 <daniela.r03@aluno.ifsc.edu.br>
2023-10-15 17:46:55 -0300

    ex4

---
+++ desafio-7dias-java/src/Exercicios/Ex4.java
@@ -0,0 +1,19 @@
+package Exercicios;
+
+public class Ex4 {
+
+	public static void main(String[] args) {
+		
+		        int[] numerosDoArray = { 3, 4, 9, 23, 33, 77 };
+		        int somaDosNumeros = 0;
+		        
+		        for (int num : numerosDoArray) {
+		            somaDosNumeros += num;
+		        }
+		        
+		        double mediaDosNumerosArray = somaDosNumeros / (double) numerosDoArray.length;
+		        
+		        System.out.println("O valor médio dos elementos do array é: " + mediaDosNumerosArray);
+	}
+
+}
