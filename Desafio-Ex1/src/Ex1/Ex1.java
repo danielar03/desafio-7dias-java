b306faa963c7a90f206b70f1c772ace37aca24f9
daniela.r03 <daniela.r03@aluno.ifsc.edu.br>
2023-10-23 22:55:17 -0300

    EX1

---
+++ Desafio-Ex1/src/Ex1/Ex1.java
@@ -0,0 +1,15 @@
+package Ex1;
+
+	import java.util.Arrays;
+
+	public class Ex1 {
+	    public static void main(String[] args) {
+	        int[] numeroArray = {67, 5, 7, 20, 0, 1};
+	        
+	        // Ordena o array numérico em ordem crescente
+	        Arrays.sort(numeroArray);
+	        
+	        System.out.println("Array numérico ordenado em ordem crescente: " + Arrays.toString(numeroArray));
+	    }
+	}
+
